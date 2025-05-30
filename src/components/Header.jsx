f6bcd765b873d080f31f34f4477a4e680abd3867
Lazaro019 <diaz.lazaro.tomas@gmail.com>
2025-05-29 23:49:09 -0300

    commit n4

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,7 +2,7 @@ function Header() {
     
     return ( 
     <header> 
-        <a href="./" target="_blank"><img src="../public/Images/logo1.png" width="150" alt="150" /></a>
+        <a href="./" target="_blank"><img src="/Images/logo1.png" width="150" alt="150" /></a>
         <div>
             <h1>Juegos x Diversion</h1>  
             <h3>Donde tu pasion por los juegos se hace realidad</h3>
